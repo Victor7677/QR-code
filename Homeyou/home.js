87132da218b2ba853c63f48ed334499951872f28
Victor <victor.cerqueira80@gmail.com>
2022-07-28 23:43:37 -0300

    segundo commit

--- Homeyou/home.js
+++
@@ -1,9 +0,0 @@
-const titulo = document.querySelectorAll('h1')
-function activetitulo(){
-    console.log(this)
-}
-
-
-titulo.forEach((item)=>{
-    item.addEventListener('click', activetitulo)
-})
